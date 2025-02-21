92d30a98fb609030f0bf895262e2c6bb33435645
tnebes <tomislav.nebes@gmail.com>
2025-02-21 07:40:46 +0100

    Add view asset details functionality
    - Implemented onView method in AssetsComponent to open asset details dialog
    - Added view button in assets component HTML with visibility icon
    - Styled view button with custom SCSS in table-component styles
    - Imported AssetDialogComponent for displaying asset details

---
+++ app/ClientApp/src/app/components/assets/asset-dialog/asset-dialog.component.ts
@@ -0,0 +1,39 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { AssetResponse } from '../../../core/models/asset.interface';
+import { MatTabsModule } from '@angular/material/tabs';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-asset-dialog',
+  template: `
+    <h2 mat-dialog-title>{{data.name}}</h2>
+    <mat-tab-group>
+      <mat-tab label="Preview">
+        <mat-dialog-content>
+            
+        </mat-dialog-content>
+      </mat-tab>
+      <mat-tab label="Metadata">
+        <mat-dialog-content>
+            <p><strong>ID:</strong> {{data.id}}</p>
+            <p><strong>Name:</strong> {{data.name}}</p>
+          <p><strong>Description:</strong> {{data.description}}</p>
+          <p><strong>Asset Path:</strong> {{data.assetPath}}</p>
+          <p><strong>Created By:</strong> {{data.createdBy}}</p>
+          <p><strong>Created At:</strong> {{data.createdAt | date}}</p>
+          <p><strong>Updated By:</strong> {{data.updatedBy}}</p>
+          <p><strong>Updated At:</strong> {{data.updatedAt | date}}</p>
+        </mat-dialog-content>
+      </mat-tab>
+    </mat-tab-group>
+    <mat-dialog-actions>
+      <button mat-raised-button mat-dialog-close class="close-button" color="primary">Close</button>
+    </mat-dialog-actions>
+  `,
+  standalone: true,
+  imports: [MatTabsModule, MatDialogModule, DatePipe]
+})
+export class AssetDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: AssetResponse) {}
+} 
